807218a69f1f71469e6d401c2b6354a138592980
oscarf23 <oscarmf23@gmail.com>
2020-05-14 16:51:53 -0500

    FINISHED DMP F.P.

--- main.js
+++ main.js
@@ -1,15 +1,81 @@
 function check() {
-    var Question1=document.SportsForm.Question1.value;
-    var Question2=document.SportsForm.Question2.value;
-    var Question3=document.SportsForm.Question3.value;
-    var Question4=document.SportsForm.Question4.value;
-    var Question5=document.SportsForm.Question5.value;
-    var Question6=document.SportsForm.Question6.value;
+
     var count=0;
 
+
+    var Question1RadioOptions=document.getElementsByName("Question1");
+    var Question1;
+    for(i = 0; i < Question1RadioOptions.length; i++) { 
+        if(Question1RadioOptions[i].checked) 
+                Question1= Question1RadioOptions[i].value;
+    }
+    console.log(Question1); 
     if(Question1== "a") {
         count++;
     }
+
+
+    var Question2RadioOptions=document.getElementsByName("Question2");
+    var Question2;
+    for(i = 0; i < Question2RadioOptions.length; i++) { 
+        if(Question2RadioOptions[i].checked) 
+                Question2= Question2RadioOptions[i].value;
+    }
+    console.log(Question2); 
+    if(Question2== "a") {
+        count++;
+    }
+
+
+    var Question3RadioOptions=document.getElementsByName("Question3");
+    var Question3;
+    for(i = 0; i < Question3RadioOptions.length; i++) { 
+        if(Question3RadioOptions[i].checked) 
+                Question3= Question3RadioOptions[i].value;
+    }
+    console.log(Question3); 
+    if(Question3== "a") {
+        count++;
+    }
+
+    var Question4RadioOptions=document.getElementsByName("Question4");
+    var Question4;
+    for(i = 0; i < Question4RadioOptions.length; i++) { 
+        if(Question4RadioOptions[i].checked) 
+                Question4= Question4RadioOptions[i].value;
+    }
+    console.log(Question4); 
+    if(Question4== "a") {
+        count++;
+    }
+
+    var Question5RadioOptions=document.getElementsByName("Question5");
+    var Question5;
+    for(i = 0; i < Question5RadioOptions.length; i++) { 
+        if(Question5RadioOptions[i].checked) 
+                Question5= Question5RadioOptions[i].value;
+    }
+    console.log(Question5); 
+    if(Question5== "a") {
+        count++;
+    }
+
+    var Question6RadioOptions=document.getElementsByName("Question6");
+    var Question6;
+    for(i = 0; i < Question6RadioOptions.length; i++) { 
+        if(Question6RadioOptions[i].checked) 
+                Question6= Question6RadioOptions[i].value;
+    }
+    console.log(Question6); 
+    if(Question6== "a") {
+        count++;
+    }
+
+    alert("You got" +count+ "correct!");
+    return true;
+
+
+    
     if(Question2=="a") {
         count++;
     }
